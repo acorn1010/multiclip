b23a520754f42f1dfeceacc4c2ae3eeef9f84123
acorn1010 <github@unallied.com>
2022-11-21 09:08:55 -0800

    Change font to Work-Sans

--- src/client/theme/SiteTheme.tsx
+++ src/client/theme/SiteTheme.tsx
@@ -1,10 +1,20 @@
 import { ThemeProvider } from "@mui/system";
 import { createTheme, CssBaseline } from "@mui/material";
 import type { PropsWithChildren } from "react";
+import {Work_Sans} from "@next/font/google";
+
+const font = Work_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--worksans-font',
+})
 
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
+  },
+  typography: {
+    fontFamily: 'var(--worksans-font)',
   },
 });
 
