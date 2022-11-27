5cf1021cc73ad1516ddb15cadbda30bdf3b9c9af
acorn1010 <github@unallied.com>
2022-11-27 12:12:56 -0800

    Add subtle background to page
    Add a heropatterns background to the page using Tailwind's colors.

--- src/client/theme/SiteTheme.tsx
+++ src/client/theme/SiteTheme.tsx
@@ -1,8 +1,10 @@
-import { ThemeProvider } from "@mui/system";
+import {GlobalStyles, ThemeProvider} from "@mui/system";
 import { createTheme, CssBaseline } from "@mui/material";
 import type { PropsWithChildren } from "react";
 import {Work_Sans} from "@next/font/google";
 
+// Initialize font. This must be assigned to a variable in order to build.
+// noinspection JSUnusedLocalSymbols
 const font = Work_Sans({
   subsets: ['latin'],
   display: 'swap',
@@ -22,6 +24,12 @@ export function SiteTheme(props: PropsWithChildren<{}>) {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+      <GlobalStyles styles={{
+        body: {
+          backgroundColor: '#18181b',
+          backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2327272a' fill-opacity='0.4'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+        },
+      }} />
       {props.children}
     </ThemeProvider>
   );
