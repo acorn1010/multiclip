ee286af410833577a5154400aea52722492e0756
acorn1010 <github@unallied.com>
2023-06-20 17:01:17 -0700

    Add Kick stream to acorn1010.com
    Add the current livestream if I'm live to acorn1010.com

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material";
 import Head from "next/head";
 import {TwitchClipsGrid} from "../client/clips/TwitchClipsGrid";
+import {useEffect, useState} from "react";
 
 export default function Home() {
   return (
@@ -12,8 +13,64 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container className='mt-4' maxWidth='xl'>
+        <KickStream />
         <TwitchClipsGrid />
       </Container>
     </>
   );
+}
+
+/** Displays the Kick livestream if currently live. */
+function KickStream() {
+  const livestream = useKickLivestream('acorn1010');
+
+  if (!livestream) {
+    return null;
+  }
+
+  const src = 'https://kick.com/acorn1010';
+  return (
+    <div>
+      <h2><a className='text-white no-underline hover:underline' href={src} target='_blank' rel='noreferrer'>🔴 I'M LIVE! - ${livestream.session_title}</a></h2>
+      <iframe
+        src={src}
+        title={livestream.session_title}
+        style={{border: 'none', width: '100%', height: 640}} />
+    </div>
+  );
+}
+
+type KickResponse = {
+  livestream?: {
+    /** Suffix for the livestream (e.g. "4654a-foony") */
+    slug: string,
+    /** Time when this was created (e.g. "2023-06-20 20:06:31") */
+    created_at: string,
+    /** Title of the livestream. */
+    session_title: string,
+    /** True if the stream is currently live. */
+    is_live: boolean,
+    /** Number of viewers currently watching. */
+    viewer_count: number,
+  },
+};
+
+function useKickLivestream(slug: string) {
+  const [response, setResponse] = useState<KickResponse>();
+  useEffect(() => {
+    const callback = async () => {
+      const response = await fetch(`https://kick.com/api/v1/channels/${slug}`);
+      const json: KickResponse = await response.json();
+      if (json) {
+        setResponse(json);
+      }
+    };
+    callback().catch(() => {});
+    const interval = setInterval(callback, 10_000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [slug]);
+
+  return response === undefined ? undefined : (response?.livestream ?? null);
 }
